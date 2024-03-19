110a19f519901cffd3ea1565b0670c9e49d8f824
MRK <yablokovandrew2003@gmail.com>
2024-03-19 14:33:34 +0100

    Tracking to path

--- _Input/_/Intermediate Artificial Intelligence Single Entity.cpp
+++ _Input/_/Intermediate Artificial Intelligence Single Entity.cpp
@@ -9,46 +9,58 @@
 std::int32_t main()
 {
     std::unordered_map<std::string , std::unordered_map<std::string , std::uintmax_t>> LTransitions;
-    std::uintmax_t LBegin;
-    std::uintmax_t LEnd;
+    std::string LBegin;
+    std::string LEnd;
     std::unordered_set<std::string> LLocations;
     std::unordered_map<std::string , std::uintmax_t> LCosts;
-    std::unordered_map<std::string , std::uintmax_t> LDistances;
+    std::unordered_map<std::string , std::vector<std::string>> LPaths;
     std::string LCurrent;
-    LTransitions["A"]["B"] = 1;
-    LTransitions["B"]["D"] = 2;
-    LTransitions["A"]["C"] = 3;
-    LTransitions["C"]["D"] = 4;
-    LBegin = 0;
-    LEnd = 3;
+    LTransitions["A"]["B"] = 9;
+    LTransitions["B"]["C"] = 8;
+    LTransitions["A"]["D"] = 1;
+    LTransitions["D"]["C"] = 2;
+    LBegin = "A";
+    LEnd = "C";
     for(std::pair<const std::string , std::unordered_map<std::string , std::uintmax_t>>& LTransition : LTransitions)
     {
         LLocations.insert(LTransition.first);
+        LCosts[LTransition.first] = std::numeric_limits<std::uintmax_t>::max();
         for(std::pair<const std::string , std::uintmax_t>& LCost : LTransition.second)
         {
             LLocations.insert(LCost.first);
+            LCosts[LCost.first] = std::numeric_limits<std::uintmax_t>::max();
         }
     }
-    std::ranges::for_each(LCosts , [](std::uintmax_t& PCost){PCost = std::numeric_limits<std::uintmax_t>::max();});
-    LCosts[LBegin] = 0;
-    std::ranges::for_each(LDistances , [](std::uintmax_t& PDistance){PDistance = std::numeric_limits<std::uintmax_t>::max();});
-    LDistances[LBegin] = 0;
+    LCosts[LBegin] = 0;\
+    LPaths[LBegin] = {};
     while(!LLocations.empty())
     {
-        LCurrent = std::ranges::min(LLocations);
+        LCurrent
+        =
+        std::ranges::min_element
+        (
+            LCosts
+            ,
+            [&]
+            (std::pair<const std::string , std::uintmax_t>& PCost , std::pair<const std::string , std::uintmax_t>& PMinimum)
+            {return !LLocations.contains(PMinimum.first) || (LLocations.contains(PCost.first) && PCost.second < PMinimum.second);}
+        )
+        ->
+        first;
         for(std::string LLocation : LLocations)
         {
             if(LCurrent != LLocation)
             {
-                if(LTransitions[LCurrent][LLocation] != std::numeric_limits<std::uintmax_t>::max())
+                if(LTransitions[LCurrent].contains(LLocation))
                 {
                     if(LTransitions[LCurrent][LLocation] < LCosts[LLocation])
                     {
                         LCosts[LLocation] = LCosts[LCurrent] + LTransitions[LCurrent][LLocation];
                     }
-                    if(LDistances[LCurrent] < LDistances[LLocation])
+                    if(!LPaths.contains(LLocation) || LPaths[LCurrent].size() + 1 < LPaths[LLocation].size())
                     {
-                        LDistances[LLocation] = LDistances[LCurrent] + 1;
+                        LPaths[LLocation] = LPaths[LCurrent];
+                        LPaths[LLocation].push_back(LCurrent);
                     }
                 }
             }
@@ -56,7 +68,10 @@ std::int32_t main()
         LLocations.erase(std::ranges::find(LLocations , LCurrent));
     }
     std::cout << "Cost = " << LCosts[LEnd] << "\n"
-              << "Distance = " << LDistances[LEnd] << "\n";
+              << "Distance = " << LPaths[LEnd].size() << "\n"
+              << "Path = ";
+    std::ranges::for_each(LPaths[LEnd] , [](std::string& PPath){std::cout << ">" << PPath;});
+    std::cout << "\n";
     #ifdef _WIN32
         std::system("Pause");
     #endif
